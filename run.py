90e02db0d2e887097eac05c902621fd24212512e
David Lai <me@dlai0001.com>
2016-05-14 14:07:29 -0700

    simple cli placeholder for run.py

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 
 
-print "I am a python script"
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("mode", help="master or slave")
+parser.add_argument("address", help="address of master")
+
+args = parser.parse_args()
+
+if args.mode.lower() == 'master':
+    print "Starting as Locust Master"
+else:
+    print "Starting as Locust Slave"
+
